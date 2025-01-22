6de6acc688c403fa3e0352ec08b0918b9613de8d
Brian Kimutai <brianoproff@gmail.com>
2025-01-22 08:53:48 +0300

    revamp: allows user to choose the OTP delivery

--- src/components/views/auth/forgotPassword.tsx
+++ src/components/views/auth/forgotPassword.tsx
@@ -12,6 +12,8 @@ import { CircularProgress } from "@mui/material";
 import { ModalID } from "@/domain/components";
 import { RequestOTPPayload } from "@/domain/dto/input";
 
+type OTPMethod = "email" | "phone" | "";
+
 type ForgotPasswordModalContentProps = {
   loading: boolean;
   requestOTP: (payload: RequestOTPPayload) => void;
@@ -20,24 +22,40 @@ type ForgotPasswordModalContentProps = {
 
 const defaultValues = {
   email: "",
+  phone_number: "",
+  otp_method: "",
 };
 
 interface FormData {
   email: string;
+  phone_number: string;
+  otp_method: string | OTPMethod;
 }
 
 const schema = yup.object().shape({
-  email: yup.string().email("Invalid email").required("Email is required"),
+  email: yup.string().when("otp_method", {
+    is: "email",
+    then: (schema) =>
+      schema.email("Invalid email").required("Email is required"),
+    otherwise: (schema) => schema,
+  }),
+  phone_number: yup.string().when("otp_method", {
+    is: "phone",
+    then: (schema) => schema.min(10).required("Phone number is required"),
+    otherwise: (schema) => schema,
+  }),
+  otp_method: yup.string().oneOf(["email", "phone"]).required(),
 });
 
 const ForgotPasswordModalContent: FC<ForgotPasswordModalContentProps> = ({
   loading,
   requestOTP,
-  setActiveModal
+  setActiveModal,
 }) => {
   const {
     control,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     defaultValues,
@@ -46,7 +64,10 @@ const ForgotPasswordModalContent: FC<ForgotPasswordModalContentProps> = ({
   });
 
   const onSubmit = (data: FormData) => {
-    requestOTP({identifier: data?.email});
+    requestOTP({
+      identifier: data.otp_method === "email" ? data.email : data.phone_number,
+      otp_method: data.otp_method,
+    });
   };
 
   return (
@@ -65,35 +86,77 @@ const ForgotPasswordModalContent: FC<ForgotPasswordModalContentProps> = ({
       <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
         <div className="space-y-4">
           <Controller
-            name="email"
+            name="otp_method"
             control={control}
-            rules={{ required: true }}
             render={({ field: { value, onChange } }) => (
-              <FormInputAuth
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Email"
-                onChange={onChange}
+              <select
+                className="w-full p-2 border rounded-md"
                 value={value}
-                error={errors?.email?.message}
-              />
+                onChange={onChange}
+              >
+                <option value="">Select OTP Delivery Option</option>
+                <option value="email">Email</option>
+                <option value="phone">Phone Number</option>
+              </select>
             )}
           />
+
+          {watch("otp_method") === "email" && (
+            <Controller
+              name="email"
+              control={control}
+              render={({ field: { value, onChange } }) => (
+                <FormInputAuth
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  onChange={onChange}
+                  value={value}
+                  error={errors?.email?.message}
+                />
+              )}
+            />
+          )}
+
+          {watch("otp_method") === "phone" && (
+            <Controller
+              name="phone_number"
+              control={control}
+              render={({ field: { value, onChange } }) => (
+                <FormInputAuth
+                  id="phone_number"
+                  name="phone_number"
+                  type="tel"
+                  placeholder="Phone Number"
+                  onChange={onChange}
+                  value={value}
+                  error={errors?.phone_number?.message}
+                />
+              )}
+            />
+          )}
 
           <button
             type="submit"
             className="w-full bg-orange-400 text-white py-2 rounded-md hover:bg-orange-500"
           >
-            {loading ? <CircularProgress size={18} color="inherit" /> : "Continue"}
+            {loading ? (
+              <CircularProgress size={18} color="inherit" />
+            ) : (
+              "Continue"
+            )}
           </button>
         </div>
       </form>
 
       <div className="mt-6 text-center">
         <p className="text-sm text-gray-600 gap-1">
-          Go back to {" "}
-          <button onClick={() => setActiveModal(ModalID.login)} className="text-blue-500 hover:text-blue-600">
+          Go back to{" "}
+          <button
+            onClick={() => setActiveModal(ModalID.login)}
+            className="text-blue-500 hover:text-blue-600"
+          >
             Login
           </button>
         </p>
@@ -107,8 +170,13 @@ const mapStateToProps = (state: RootState) => ({
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-  setActiveModal: (modalId: ModalID) => dispatch.components.setActiveModal(modalId),
-  requestOTP: (payload: RequestOTPPayload) => dispatch.authentication.requestOTP(payload)
+  setActiveModal: (modalId: ModalID) =>
+    dispatch.components.setActiveModal(modalId),
+  requestOTP: (payload: RequestOTPPayload) =>
+    dispatch.authentication.requestOTP(payload),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ForgotPasswordModalContent); 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ForgotPasswordModalContent);
