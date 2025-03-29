23e4adaab80c8599f20c7986e983ab204300aacb
Brian Kimutai Kiboino <brianoproff@gmail.com>
2025-03-29 10:39:07 +0300

    fix: fixes image visibility for product tags (#56)

--- src/components/products/homePage/tags/index.tsx
+++ src/components/products/homePage/tags/index.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { FC, useRef } from "react";
+import React, { FC, useRef, useState, useEffect } from "react";
 import { ProductTag } from "@/domain/dto/output";
 import { ChevronRight } from "lucide-react";
 import { connect } from "react-redux";
@@ -9,29 +7,52 @@ import { ComprehensiveProductFilters } from "@/domain/dto/product.input";
 import useDeviceType from "@/lib/hooks/useDeviceType";
 import { DeviceType } from "../../../../domain/constants";
 import Image from "next/image";
+import { formatProductFilters } from "@/utilities";
+import { ModalID } from "@/domain/components";
 
 type ProductTagsProps = {
   isProcessingRequest: boolean;
-  toggleCategoryFilter: (categoryId: string) => void;
+  toggleTagFilter: (tagIg: string) => void;
   comprehensiveProductFilters: ComprehensiveProductFilters;
   productTags: Array<ProductTag>;
+  fetchFilteredProducts: (filters: ComprehensiveProductFilters) => void;
 };
 
 const ProductsTags: FC<ProductTagsProps> = ({
-  toggleCategoryFilter,
+  toggleTagFilter,
   comprehensiveProductFilters,
   productTags,
   isProcessingRequest,
+  fetchFilteredProducts,
 }) => {
   const deviceType = useDeviceType();
   const categoriesContainerRef = useRef<HTMLDivElement>(null);
+  const [isScrollable, setIsScrollable] = useState(false);
 
-  const Shimmer = () => (
-    <div className="flex flex-col items-center gap-1 md:min-w-[80px] flex-shrink-0 animate-pulse">
-      <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-      <div className="w-12 h-4 bg-gray-300 rounded"></div>
-    </div>
-  );
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (categoriesContainerRef.current) {
+        setIsScrollable(
+          categoriesContainerRef.current.scrollWidth >
+            categoriesContainerRef.current.clientWidth
+        );
+      }
+    };
+
+    checkOverflow();
+    window.addEventListener("resize", checkOverflow);
+
+    return () => window.removeEventListener("resize", checkOverflow);
+  }, [productTags]);
+
+  const handleScroll = () => {
+    if (categoriesContainerRef.current) {
+      categoriesContainerRef.current.scrollBy({
+        left: 100,
+        behavior: "smooth",
+      });
+    }
+  };
 
   return (
     <div>
@@ -41,7 +62,13 @@ const ProductsTags: FC<ProductTagsProps> = ({
             <div className="flex justify-evenly items-center gap-6 overflow-x-auto no-scrollbar">
               {[...Array(deviceType === DeviceType.mobile ? 10 : 51)].map(
                 (_, index) => (
-                  <Shimmer key={index} />
+                  <div
+                    key={index}
+                    className="flex flex-col items-center gap-1 md:min-w-[80px] flex-shrink-0 animate-pulse"
+                  >
+                    <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+                    <div className="w-12 h-4 bg-gray-300 rounded"></div>
+                  </div>
                 )
               )}
             </div>
@@ -51,10 +78,7 @@ const ProductsTags: FC<ProductTagsProps> = ({
         <div>
           {productTags && productTags?.length > 0 ? (
             <div className="py-1 md:py-2 bg-white px-2 md:px-3 lg:px-4">
-              <div
-                className="flex items-center gap-8 py-4
-      px-4 md:px-7"
-              >
+              <div className="flex justify-center items-center gap-8 py-4 px-4 md:px-7">
                 <div
                   ref={categoriesContainerRef}
                   className="flex justify-evenly items-center gap-6 overflow-x-auto no-scrollbar"
@@ -63,31 +87,39 @@ const ProductsTags: FC<ProductTagsProps> = ({
                     <span
                       key={index}
                       className="flex flex-col items-center gap-1 md:min-w-[80px] flex-shrink-0 cursor-pointer"
-                      onClick={() => toggleCategoryFilter(tag?.id)}
+                      onClick={() => {
+                        toggleTagFilter(tag?.id);
+                        if (
+                          !comprehensiveProductFilters?.tag ||
+                          (comprehensiveProductFilters?.tag &&
+                            comprehensiveProductFilters?.tag !== tag?.id)
+                        ) {
+                          fetchFilteredProducts({ tag: tag?.id });
+                        }
+                      }}
                     >
                       <div className="flex items-center justify-center">
                         <span
-                          className={`${`${
-                            comprehensiveProductFilters?.categories?.includes(
-                              tag?.id
-                            )
+                          className={`${
+                            comprehensiveProductFilters?.tag &&
+                            comprehensiveProductFilters?.tag === tag?.id
                               ? "text-primary"
                               : "text-gray-500"
-                          }`}`}
+                          }`}
                         >
                           <Image
-                            width={10}
-                            height={10}
+                            width={30}
+                            height={30}
                             src={tag?.icon as string}
                             alt="Tag Icon"
+                            unoptimized={true}
                           />
                         </span>
                       </div>
                       <span
                         className={`text-sm md:text-xs text-center ${
-                          comprehensiveProductFilters?.categories?.includes(
-                            tag?.id
-                          )
+                          comprehensiveProductFilters?.tag &&
+                          comprehensiveProductFilters?.tag === tag?.id
                             ? "text-primary"
                             : "text-gray-500"
                         }`}
@@ -97,24 +129,17 @@ const ProductsTags: FC<ProductTagsProps> = ({
                     </span>
                   ))}
                 </div>
-                <button
-                  className="bg-white hover:text-primary flex-shrink-0"
-                  onClick={() => {
-                    if (categoriesContainerRef.current) {
-                      categoriesContainerRef.current.scrollBy({
-                        left: 100,
-                        behavior: "smooth",
-                      });
-                    }
-                  }}
-                >
-                  <ChevronRight className="w-8 h-8" />
-                </button>
+                {isScrollable && (
+                  <button
+                    className="bg-white hover:text-primary flex-shrink-0"
+                    onClick={handleScroll}
+                  >
+                    <ChevronRight className="w-8 h-8" />
+                  </button>
+                )}
               </div>
             </div>
-          ) : (
-            <></>
-          )}
+          ) : null}
         </div>
       )}
     </div>
@@ -133,8 +158,22 @@ const mapStateToProps = (state: RootState) => {
 };
 
 const mapDispatchToProps = (dispatch: any) => ({
-  toggleCategoryFilter: (categoryId: string) =>
-    dispatch.products.toggleCategoryFilter(categoryId),
+  toggleTagFilter: (tagId: string) => dispatch.products.toggleTagFilter(tagId),
+  fetchFilteredProducts: (filters: ComprehensiveProductFilters) => {
+    const formattedFilters = formatProductFilters(filters);
+    const finalFilters = {
+      ...formattedFilters,
+      page: 1,
+      page_size: 5,
+      tag: filters?.tag,
+    } as any;
+    dispatch.products.getPopularProducts(finalFilters);
+    dispatch.products.getNearByProducts(finalFilters);
+    dispatch.products.getPopularProducts(finalFilters);
+    dispatch.products.getRecommendedProducts(finalFilters);
+    dispatch.products.getUpcomingProducts(finalFilters);
+    dispatch.components.setActiveModal(ModalID.none);
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductsTags);
